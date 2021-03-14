37a4f48b68e918f0809e0a353ab9dd8ff2779553
Matt <mattcharlesh@gmail.com>
2021-03-14 12:09:08 -0400

    inital setup: gitignore, client & server

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import Home from "./pages/home"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <Home/>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
