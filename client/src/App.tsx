9f7775566ad5b3afdf2dd78d0b602130743e4fb2
Matt <mattcharlesh@gmail.com>
2021-03-15 21:46:38 -0400

    added simple navbar
    - corrected errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,10 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import Home from "./pages/home"
+import * as React from 'react';
+import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react';
+
+import Post from './pages/Post';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <Home/>
-      </Grid>
-    </Box>
+    <Post />
   </ChakraProvider>
-)
+);
---
+++ client/src/pages/Post.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Flex, Text, Link, Stack } from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
+
+function Post() {
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      mb={8}
+      py={6}
+      px={10}
+      bg="teal.300"
+    >
+      <Text fontSize="3xl">Zufa Rental</Text>
+      <Flex
+        justifySelf="flex-end"
+        alignItems="center"
+      >
+        <Link fontSize="2xl" pr={6} to="/">Home</Link>
+        <Link fontSize="2xl" pr={6} to="/">Login</Link>
+        <Link fontSize="2xl" pr={6} to="/">Signup</Link>
+        <ColorModeSwitcher justifySelf="flex-end" />
+      </Flex>
+      
+    </Flex>
+  );
+}
+
+export default Post;
