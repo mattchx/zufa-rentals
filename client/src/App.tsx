016c6f5a65d49a12edcc36fe0aff465e3e17f84e
Matt <mattcharlesh@gmail.com>
2021-04-20 18:01:55 -0400

    cra cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,8 @@
 import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import Home from "./pages/home"
+
+
+import Home from "./pages/Home"
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
         <Home/>
-      </Grid>
-    </Box>
-  </ChakraProvider>
 )
