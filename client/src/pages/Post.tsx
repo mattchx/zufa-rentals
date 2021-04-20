c3bb12312ba5026ff01e761b9c73e9ee63fb1e4d
Matt <mattcharlesh@gmail.com>
2021-04-20 18:51:58 -0400

    added image and updated post.tsx

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -4,8 +4,15 @@ import styled from '@emotion/styled'
 
 const Post = () => {
   return (
+      <>
     <h2>Zufa Rentals</h2>
+    <img width='44' alt='house' src="../assets/small-one-storey-house-medium.jpeg"/>
+    </>
   );
 }
 
 export default Post;
+
+const Img = styled.img`
+  color: turquoise;
+`
