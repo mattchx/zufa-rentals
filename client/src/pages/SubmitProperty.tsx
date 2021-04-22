c0158c15a88d9de2e15339d7a42948757c26a132
Matt <mattcharlesh@gmail.com>
2021-04-21 20:05:15 -0400

    Create SubmitProperty.tsx

---
+++ client/src/pages/SubmitProperty.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const SubmitProperty = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SubmitProperty
